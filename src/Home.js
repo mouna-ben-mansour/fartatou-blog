c73fca71f05e55eda988fc8c8be74a92a77902e7
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-05 14:40:29 +0200

    set a reference that invoke the function for us

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = () =>{
+        console.log('hello, fartatous');
+    }   
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
