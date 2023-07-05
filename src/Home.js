cb7a8152290ce817b4724e451ccf335af349c0a4
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-05 14:56:20 +0200

    event object

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
 const Home = () => {
-    const handleClick = () =>{
+    const handleClick = (e) =>{
         console.log('hello, fartatous');
+        console.log(e);
     }   
 
-    const handleClickAgain = (name) =>{
+    const handleClickAgain = (name,e) =>{
         console.log('hello,'+ name);
+        console.log(e.target);
     } 
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={()=> handleClickAgain('Mouna')}>Click me again</button> {/* add argument without invoke the function so we use anonymous function */}
+            <button onClick={(e)=> handleClickAgain('Mouna',e)}>Click me again</button> {/* add argument without invoke the function so we use anonymous function */}
         </div>
      );
 }
