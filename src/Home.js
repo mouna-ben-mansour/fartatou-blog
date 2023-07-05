f670fa6774be782b5264f591be006f67cfccf06f
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-05 14:52:36 +0200

    add argument without invoke the function so we use anonymous function

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,15 @@ const Home = () => {
         console.log('hello, fartatous');
     }   
 
+    const handleClickAgain = (name) =>{
+        console.log('hello,'+ name);
+    } 
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={()=> handleClickAgain('Mouna')}>Click me again</button> {/* add argument without invoke the function so we use anonymous function */}
         </div>
      );
 }
