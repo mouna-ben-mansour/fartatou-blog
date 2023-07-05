0f4020dcaf03a1804bf109eece336138ada77bd8
mouna ben mansour <benmansour.mouna2@gmail.com>
2023-07-05 14:05:41 +0200

    multiple component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  const title = 'welcome to the new blog';
-  const likes =50;
-  const person = { name:'Sarra', age:'31'};
-  const linkGoogle= "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App component</h1>
-        <h2>{ title }</h2>
-        <p>Liked { likes } times</p>
-        <p>Name: { person.name } <br></br>Age: { person.age }</p>
-        <p>{ 10 }</p>
-        <p >{ 'Hello, fartatous' }</p>
-        <p >{ [1,2,3,4,5] }</p>
-        <a href="http://www.google.com" >google</a>
-        <br></br>
-        <a href={ linkGoogle } >google site</a>
+        <Home />
       </div>
     </div>
   );
